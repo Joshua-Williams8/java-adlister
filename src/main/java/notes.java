e7ffbd4d90b7bc74e0f6eecbca4786bc602a7aa2
Joshua-Williams8 <joshua.n.williams8@gmail.com>
2021-07-04 10:39:21 -0500

    review and refactoring

---
+++ src/main/java/notes.java
@@ -0,0 +1,8 @@
+public class notes {
+//  cookies are so we can save our login information as a cookie, so when the server recieves it it uses it as a unique ID
+
+//  cookies are saved in the browser.
+
+//  Session
+
+}
